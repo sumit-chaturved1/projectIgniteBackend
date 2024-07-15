24c0281870421263268f1a307e9be5fc6be8d16f
Sumit <Chaturvedisumit338@gmail.com>
2024-07-15 20:38:22 +0530

    test

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://sprightly-speculoos-089ad5.netlify.app",
+    origin: "https://projectignite.netlify.app",
     credentials: true, // Allow credentials
   })
 );
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://sprightly-speculoos-089ad5.netlify.app"
+    "https://projectignite.netlify.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
